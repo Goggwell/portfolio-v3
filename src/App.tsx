b652870c8029b256add74d07c9dd7ea3efdc66b1
samuel-bd <samuely@frisseblikken.com>
2022-01-26 15:44:30 +0700

    basic theme toggle implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas, faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { fab, faGithub } from '@fortawesome/free-brands-svg-icons';
 import Header from './components/Header';
+import useLocalStorage from 'use-local-storage';
 
 library.add(fab, fas, faGithub, faGlobe);
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light'
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
   return (
-    <>
+    <div className='app' data-theme={theme}>
       <div className='bg'></div>
+      <button onClick={switchTheme}>SWITCH</button>
       <Header />
-    </>
+    </div>
   );
 }
 
